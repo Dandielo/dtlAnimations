3da8bb661ba86f318befab1d2d87200ac33d9d09
dandielo <daniel.penkala@gmail.com>
2013-05-07 11:34:07 +0200

    Update for CB 1.5.2

--- src/main/java/net/dandielo/animation/AnimationFrame.java
+++ src/main/java/net/dandielo/animation/AnimationFrame.java
@@ -3,12 +3,12 @@ package net.dandielo.animation;
 import java.util.List;
 
 import net.dandielo.bukkit.DtlAnimations;
-import net.minecraft.server.v1_4_R1.EntityPlayer;
-import net.minecraft.server.v1_4_R1.Packet52MultiBlockChange;
+import net.minecraft.server.v1_5_R3.EntityPlayer;
+import net.minecraft.server.v1_5_R3.Packet52MultiBlockChange;
 
 import org.bukkit.Location;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.craftbukkit.v1_4_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_5_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 public class AnimationFrame implements Cloneable {
